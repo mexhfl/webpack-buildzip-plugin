62afefbd8d9bbcfc92785660e28afae7ded5651a
Administrator <wjd002@126.com>
2023-03-30 16:35:01 +0800

    add sftp upload

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # webpack-buildzip-plugin
-用于打包后自动化构建压缩包
+> 用于打包后自动化构建压缩包，支持打包后自动上传服务器并解压
 
 ## 使用方法
 ```
@@ -19,6 +19,29 @@ npm install webpack-buildzip-plugin --save-dev
     ]
   }
 ```
-```
 运行npm run build完成后，自动在输出目录中创建压缩包
+
+## 自动上传服务器并解压
+> 自动上传功能是基于sftp，需要填写主机账号密码，为保证安全，可将账号密码存于本地或项目以外，再通过require的方式引入到webpack配置文件
+
+``` javascript
+
+  //webpack配置文件中
+  
+  new WebpackBuildZipPlugin({
+    filename: 'build.zip', //[非必须] 设置压缩包的文件名，如果不设置，默认为 'output.zip'
+    exclude: ['*.map'],   //[非必须] 设置排除文件，按照glob的规则
+    
+    //如需配置自动上传服务器，可加入以下配置，如果不配置sshConfig，则只打包不上传服务器
+    
+    sshConfig: {
+      host: 'ip',
+      port: 22,
+      username: '用户名',
+      password: '密码',
+    },
+    remotePath: '/www', // 上传到服务器的目标目录
+    unzipCommand: `unzip -o -d /www`, // 自定义解压命令
+    
+  })
 ```
--- src/index.js
+++ src/index.js
@@ -1,51 +1,105 @@
 const archiver = require('archiver');
 const fs = require('fs');
 const path = require('path');
+const { Client } = require('ssh2');
 
 class WebpackBuildZipPlugin {
-    constructor(options = {}) {
-        this.options = options;
-        this.exclude = options.exclude || [];
-    }
-
-    apply(compiler) {
-        // 在 'done' 钩子里注册回调函数
-        compiler.hooks.done.tapAsync('WebpackBuildZipPlugin', async (stats, callback) => {
-            const {path: outputPath} = stats.compilation.options.output;
-            const archiveName = this.options.filename || 'output.zip';
-            // 创建一个文件以保存压缩包
-            const output = fs.createWriteStream(path.join(outputPath, archiveName));
-            const archive = archiver('zip', {
-                zlib: {level: 9}, // 设置压缩级别
-            });
-
-            // 监听压缩过程中的错误
-            archive.on('error', (err) => {
-                throw err;
-            });
-
-            // 将压缩包内容写入文件
-            archive.pipe(output);
-
-            // 使用 'exclude' 选项来过滤文件，并将符合条件的文件添加到压缩包中
-            archive.glob('**/*', {
-                cwd: outputPath,
-                ignore: this.exclude,
-            });
-
-            // 将构建输出目录中的所有文件添加到压缩包中
-            // archive.directory(outputPath, false);
-
-            // 完成压缩包的创建
-            archive.finalize();
-
-            // 监听 'close' 事件，以确保压缩完成后再调用回调函数
-            output.on('close', () => {
-                console.log(`Build '${archiveName}' in '${outputPath}'`);
-                callback();
-            });
+  constructor(options = {}) {
+    this.options = options;
+    this.exclude = options.exclude || [];
+    this.sshConfig = options.sshConfig || null;
+    this.remotePath = options.remotePath || '~';
+    this.unzipCommand = options.unzipCommand || `unzip -o -d ${this.remotePath}`;
+  }
+  
+  async uploadAndUnzip(archiveName, localPath) {
+    return new Promise(async (resolve, reject) => {
+      const conn = new Client();
+      conn
+      .on('ready', () => {
+        conn.sftp(async (err, sftp) => {
+          if (err) {
+            reject(err);
+          }
+          
+          const remotePath = path.join(this.remotePath, archiveName).replace(/\\/g, '/');
+          sftp.fastPut(localPath, remotePath, (err) => {
+            if (err) {
+              reject(err);
+            } else {
+              console.log(`Uploaded '${archiveName}' to '${remotePath}'`);
+              conn.exec(`${this.unzipCommand} ${remotePath}`, (err, stream) => {
+                if (err) {
+                  reject(err);
+                } else {
+                  console.log(`Unzipped '${archiveName}' at '${remotePath}'`);
+                  stream
+                  .on('close', () => {
+                    conn.end();
+                    resolve();
+                  })
+                  .on('data', (data) => {
+                    console.log('STDOUT: ' + data);
+                  })
+                  .stderr.on('data', (data) => {
+                    console.log('STDERR: ' + data);
+                  });
+                }
+              });
+            }
+          });
         });
-    }
+      })
+      .connect(this.sshConfig);
+    });
+  }
+  
+  apply(compiler) {
+    // 在 'done' 钩子里注册回调函数
+    compiler.hooks.done.tapAsync('WebpackBuildZipPlugin', async (stats, callback) => {
+      const { path: outputPath } = stats.compilation.options.output;
+      const archiveName = this.options.filename || 'output.zip';
+      // 创建一个文件以保存压缩包
+      const output = fs.createWriteStream(path.join(outputPath, archiveName));
+      const archive = archiver('zip', {
+        zlib: { level: 9 }, // 设置压缩级别
+      });
+      
+      // 监听压缩过程中的错误
+      archive.on('error', (err) => {
+        throw err;
+      });
+      
+      // 将压缩包内容写入文件
+      archive.pipe(output);
+      
+      // 使用 'exclude' 选项来过滤文件，并将符合条件的文件添加到压缩包中
+      archive.glob('**/*', {
+        cwd: outputPath,
+        ignore: this.exclude,
+      });
+      
+      // 将构建输出目录中的所有文件添加到压缩包中
+      // archive.directory(outputPath, false);
+      
+      // 完成压缩包的创建
+      archive.finalize();
+      
+      // 监听 'close' 事件，以确保压缩完成后再调用回调函数
+      output.on('close', async () => {
+        console.log(`Created '${archiveName}' in '${outputPath}'`);
+        
+        if (this.sshConfig) {
+          try {
+            await this.uploadAndUnzip(archiveName, path.join(outputPath, archiveName));
+          } catch (err) {
+            console.error('Error uploading or unzipping the file:', err);
+          }
+        }
+        callback();
+      });
+    });
+  }
 }
 
 module.exports = WebpackBuildZipPlugin;
